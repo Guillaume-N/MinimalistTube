984307dc56d3fa5366db618071b6eb7cc624d64f
Guillaume <noel.guillaume@live.fr>
2018-11-02 14:01:59 +0100

    added a link to Home on title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <section className="hero is-dark">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">Minimalist Tube</h1>
+          <h1 className="title">
+            <Link to="/">Minimalist Tube</Link>
+          </h1>
           <h2 className="subtitle">YouTube without distractions</h2>
         </div>
       </div>
