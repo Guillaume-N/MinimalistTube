9e4cec95fb32a5559ca16be77e56e695f7621e80
Guillaume <noel.guillaume@live.fr>
2018-11-03 11:06:36 +0100

    enhanced text&design + favicon

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -5,8 +5,7 @@ const WelcomeMessage = () => {
     <div className="columns">
       <div className="column is-8 is-offset-2 has-text-centered">
         <div className="notification">
-          <strong>Minimalist Tube</strong> is a clean YouTube clone uncluttered
-          from all its distractions, so you can focus on what matters.
+          <strong>Start looking for marvellous videos now!</strong>
         </div>
       </div>
     </div>
